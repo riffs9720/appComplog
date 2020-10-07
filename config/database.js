80d4f6d8ff130fce8edf5f5d0ff8f9eac4ab679b
riffs9720 <ricardo.silva@saojudas.br>
2020-10-07 14:43:15 -0300

    Iniciando o sistema

--- config/database.js
+++ config/database.js
@@ -11,23 +11,6 @@ import Url from 'url-parse';
 const DATABASE_URL = new Url(Env.get('DATABASE_URL'));
 
 export const connection = Env.get('DB_CONNECTION', 'pg');
-export const sqlite = {
-  client: 'sqlite3',
-  connection: {
-    filename: Helpers.databasePath(`${Env.get('DB_DATABASE', 'development')}.sqlite`)
-  },
-  useNullAsDefault: true
-};
-export const mysql = {
-  client: 'mysql',
-  connection: {
-    host: Env.get('DB_HOST', 'localhost'),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', 'root'),
-    password: Env.get('DB_PASSWORD', ''),
-    database: Env.get('DB_DATABASE', 'adonis')
-  }
-};
 
 export const pg = {
   client: 'pg',
